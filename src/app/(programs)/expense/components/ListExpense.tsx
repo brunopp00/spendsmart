761704452815ff77f4948d3df491a94467d2bb57
brunof <brunof@unimedvaledocai.com.br>
2024-01-23 08:18:07 -0300

    add: Componets

---
+++ src/app/(programs)/expense/components/ListExpense.tsx
@@ -0,0 +1,67 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { format } from 'date-fns'
+import { DeleteExpense } from './DeleteExpense'
+
+interface ListExpenseProps {
+  list: {
+    id: number
+    name: string
+    description: string | null
+    date: Date
+    amount: number
+    createdAt: Date
+    userId: number
+  }[]
+  sumOfValues: number
+}
+
+export const ListExpense = ({ list, sumOfValues }: ListExpenseProps) => {
+  return (
+    <ScrollArea className="h-[650px]">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Categories Names</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {list.map((item) => (
+            <TableRow key={item.id} className="dark:text-white">
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell>{format(item.date, 'dd/MM/yyyy')}</TableCell>
+              <TableCell>
+                R$ {item.amount ? item.amount.toFixed(2) : null}
+              </TableCell>
+              <DeleteExpense id={item.id} />
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>Total</TableCell>
+            <TableCell />
+            <TableCell />
+            <TableCell>
+              R$ {sumOfValues ? sumOfValues.toFixed(2) : '00.00'}
+            </TableCell>
+            <TableCell />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </ScrollArea>
+  )
+}
--- src/app/(programs)/expense/page.tsx
+++ src/app/(programs)/expense/page.tsx
@@ -1,20 +1,9 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
-import { useUserStore } from '@/store/user'
-import { format } from 'date-fns'
-import { FormCreateExpense } from './components/FormCreateExpense'
-import { DeleteExpense } from './components/DeleteExpense'
 import { prisma } from '@/lib/prisma'
-import PDFPage from './pdf'
-import { ScrollArea } from '@/components/ui/scroll-area'
+import { useUserStore } from '@/store/user'
+import { FormCreateExpense } from './components/FormCreateExpense'
 import { ImportRecurring } from './components/ImportRecurring'
+import PDFPage from './pdf'
+import { ListExpense } from './components/ListExpense'
 
 export default async function ExpenseList() {
   const {
@@ -42,43 +31,7 @@ export default async function ExpenseList() {
         </div>
       </div>
       <hr />
-      <ScrollArea className="h-[650px]">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Categories Names</TableHead>
-              <TableHead>Description</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Amount</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {list.map((item) => (
-              <TableRow key={item.id} className="dark:text-white">
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.description}</TableCell>
-                <TableCell>{format(item.date, 'dd/MM/yyyy')}</TableCell>
-                <TableCell>
-                  R$ {item.amount ? item.amount.toFixed(2) : null}
-                </TableCell>
-                <DeleteExpense id={item.id} />
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell>Total</TableCell>
-              <TableCell />
-              <TableCell />
-              <TableCell>
-                R$ {sumOfValues ? sumOfValues.toFixed(2) : '00.00'}
-              </TableCell>
-              <TableCell />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </ScrollArea>
+      <ListExpense sumOfValues={sumOfValues} list={list} />
     </div>
   )
 }
